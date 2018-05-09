fea4c3a4069fea0d70dd7dfe991a1586461e5b3c
Secaly <kevin.ledieu.fr@gmail.com>
2018-05-09 15:16:27 +0200

    add PostgreSQL

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const discord = require('discord.js');
-
+const { Client } = require('pg');
 const client = new discord.Client();
+
+const database = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+database.connect();
+
+database.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  database.end();
+});
 
 client.on('ready', function() {
   console.log('Bot ready');
