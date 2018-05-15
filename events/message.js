a9707306a9c8357ead9f4287dd1b459ec338e5fa
Secaly <kevin.ledieu.fr@gmail.com>
2018-05-15 16:01:53 +0200

    change check event

--- events/message.js
+++ events/message.js
@@ -1,7 +1,9 @@
 exports.run = (client, message) => {
+
   if (message.author.bot) return;
 
-  if (message.content.indexOf('s.' === 0)) {
+  if (message.content.indexOf('s.')  === 0) {
+
     const args = message.content.slice(2).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
